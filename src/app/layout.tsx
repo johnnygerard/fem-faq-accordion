576f9721aa8675296be7837c32907244992115dd
Johnny Gérard <john@jgerard.dev>
2024-11-26 08:09:25 +0100

    feat: replace and configure fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Work_Sans } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-work-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${workSans.variable} antialiased font-sans`}>
         {children}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,32 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["var(--font-work-sans)"],
+      },
+      fontSize: {
+        body: [
+          "0.875rem",
+          {
+            lineHeight: "150%",
+            fontWeight: "400",
+          },
+        ],
+        title: [
+          "1rem",
+          {
+            lineHeight: "normal",
+            fontWeight: "600",
+          },
+        ],
+        display: [
+          "2rem",
+          {
+            lineHeight: "normal",
+            fontWeight: "700",
+          },
+        ],
+      },
     },
   },
   plugins: [],
