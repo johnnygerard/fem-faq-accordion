434f29733d6b00331908622b4269bf414d7a62f7
Johnny Gérard <john@jgerard.dev>
2024-11-26 07:18:06 +0100

    refactor: use SVG components

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -1,25 +1,14 @@
 import type { Faq } from "@/types/faq";
-import Image from "next/image";
+import IconPlus from "@/components/icon-plus";
+import IconMinus from "@/components/icon-minus";
 
 export default function Faq({ question, answer }: Faq) {
   return (
     <details className="group" name="faq">
       <summary className="flex justify-between items-center list-none">
         <span>{question}</span>
-        <Image
-          className="group-open:hidden"
-          src="/images/icon-plus.svg"
-          alt="Show answer"
-          width={30}
-          height={30}
-        />
-        <Image
-          className="hidden group-open:block"
-          src="/images/icon-minus.svg"
-          alt="Hide answer"
-          width={30}
-          height={30}
-        />
+        <IconPlus className="group-open:hidden" />
+        <IconMinus className="hidden group-open:block" />
       </summary>
       <p>{answer}</p>
     </details>
