d17884d74a8f3e1d24cb930cfb293f2d981f2967
Johnny Gérard <john@jgerard.dev>
2024-11-26 16:06:57 +0100

    feat: set page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import FaqAccordion from "@/components/faq-accordion";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen place-items-center">
+    <div className="grid min-h-screen place-items-center px-[1.5rem] py-[2rem] md:py-[3rem]">
       <FaqAccordion />
     </div>
   );
