922db371b0a7173aaff603cb9eb57c76c84620a1
Johnny Gérard <john@jgerard.dev>
2024-11-26 16:10:37 +0100

    feat: add border-radius to FaqAccordion

--- src/components/faq-accordion.tsx
+++ src/components/faq-accordion.tsx
@@ -4,7 +4,7 @@ import IconStar from "@/components/icon-star";
 
 export default function FaqAccordion() {
   return (
-    <div className="w-full bg-white shadow-[0px_32px_56px_0px_rgba(80,0,118,0.10)] max-md:max-w-[24rem] md:w-[37.5rem]">
+    <div className="w-full rounded-[0.5rem] bg-white shadow-[0px_32px_56px_0px_rgba(80,0,118,0.10)] max-md:max-w-[24rem] md:w-[37.5rem] md:rounded-[1rem]">
       <div className="flex items-center gap-[1.5rem]">
         <IconStar />
         <h1 className="text-display text-dark-purple">FAQs</h1>
