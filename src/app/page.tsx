682c5704dffbe7c5fb6296348f45f9a3cebc0007
Johnny Gérard <john@jgerard.dev>
2024-11-26 08:21:15 +0100

    feat: add star icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { faqs } from "@/app/faqs";
 import Faq from "@/components/faq";
+import IconStar from "@/components/icon-star";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-display">FAQs</h1>
+      <div className="flex items-center gap-[1.5rem]">
+        <IconStar />
+        <h1 className="text-display">FAQs</h1>
+      </div>
       <ul>
         {faqs.map((faq) => (
           <li key={faq.question}>
