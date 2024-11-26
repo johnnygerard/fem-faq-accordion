4cdb3d4d66d02641c9e13c0ce55a8c3b9831b5c4
Johnny Gérard <john@jgerard.dev>
2024-11-26 16:07:12 +0100

    feat: set FaqAccordion width

--- src/components/faq-accordion.tsx
+++ src/components/faq-accordion.tsx
@@ -4,7 +4,7 @@ import IconStar from "@/components/icon-star";
 
 export default function FaqAccordion() {
   return (
-    <div className="bg-white shadow-[0px_32px_56px_0px_rgba(80,0,118,0.10)]">
+    <div className="w-full bg-white shadow-[0px_32px_56px_0px_rgba(80,0,118,0.10)] max-md:max-w-[24rem] md:w-[37.5rem]">
       <div className="flex items-center gap-[1.5rem]">
         <IconStar />
         <h1 className="text-display text-dark-purple">FAQs</h1>
