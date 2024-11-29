a2469542ac8cd9208c6fbb134f7ea3995e714e71
Johnny Gérard <john@jgerard.dev>
2024-11-29 17:29:47 +0100

    refactor: move centering div to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import FaqAccordion from "@/components/faq-accordion";
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen place-items-center px-[1.5rem] py-[2rem] dt:py-[3rem]">
-      <FaqAccordion />
-    </div>
-  );
+  return <FaqAccordion />;
 }
