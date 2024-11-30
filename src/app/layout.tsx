c1c62e7d977de5e4252b970607a2e05bfd1e8cc4
Johnny Gérard <john@jgerard.dev>
2024-11-30 03:36:46 +0100

    feat: add noscript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,38 @@ export default function RootLayout({
         <div className="grid min-h-screen place-items-center px-[1.5rem] py-[2rem] dt:py-[3rem]">
           {children}
         </div>
+        <noscript>
+          <div
+            style={{
+              position: "fixed",
+              zIndex: 1000,
+              top: 0,
+              left: 0,
+              right: 0,
+              padding: "1rem",
+              backgroundColor: "#fff4f4",
+              color: "#d32f2f",
+              borderBottom: "2px solid currentColor",
+              textAlign: "center",
+            }}
+            role="alert"
+          >
+            <p>
+              JavaScript is required for this website to function properly.
+              Please ensure that it is supported and enabled in your browser
+              settings.
+              <br />
+              To learn more, check out{" "}
+              <a
+                style={{ textDecorationLine: "underline", color: "LinkText" }}
+                href="https://enable-javascript.com/"
+              >
+                How to enable JavaScript in your browser
+              </a>
+              .
+            </p>
+          </div>
+        </noscript>
       </body>
     </html>
   );
