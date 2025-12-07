757bc1714a10bb2de5f7bcd2deca8e7a6b8cd2e6
Johnny Gérard <john@jgerard.dev>
2025-12-07 19:11:23 +0100

    feat: update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,24 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import Link from "next/link";
+import type { FC } from "react";
 
 export const metadata: Metadata = {
   title: "Page Not Found",
 };
 
-const NotFound = () => {
+const NotFound: FC = () => {
   return (
-    <div className="grid min-h-screen place-items-center">
-      <div className="text-center">
-        <hgroup>
-          <p>404</p>
-          <h1>Page Not Found</h1>
-        </hgroup>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link href="/">Back to Home</Link>
-      </div>
+    <div className="flex flex-col items-start gap-4">
+      <h1 className="text-display text-dark-purple dt:text-display-dt">
+        404 Not Found
+      </h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link
+        className="text-title text-dark-purple transition-colors hover:text-pink dt:text-title-dt"
+        href="/"
+      >
+        Back to Home
+      </Link>
     </div>
   );
 };
